dda26482282f41962d521c3202768cf5dfed1b65
Dodocool <96233000+Dodocool@users.noreply.github.com>
2022-01-02 22:44:36 +0100

    Add files via upload

--- Quiz/index.js
+++ Quiz/index.js
@@ -18,212 +18,95 @@ const questions = [
     },
 
     {
-        question: "Who was the first President of USA ?",
+        question: "Comment s'appelle le doudou de Peppa ?",
         optionA: "Donald Trump",
-        optionB: "Barack Obama",
-        optionC: "Abraham Lincoln",
-        optionD: "George Washington",
-        correctOption: "optionD"
+        optionB: "Teddy",
+        optionC: "Jérémy",
+        optionD: "Doudou",
+        correctOption: "optionB"
     },
 
     {
-        question: "30 days has ______ ?",
-        optionA: "January",
-        optionB: "December",
-        optionC: "June",
-        optionD: "August",
+        question: "Comment s'appelle le doudou de George ?",
+        optionA: "Teddy",
+        optionB: "Jean",
+        optionC: "Monsieur Dinosaure",
+        optionD: "Mister Dinosaure",
         correctOption: "optionC"
     },
 
     {
-        question: "How manay hours can be found in a day ?",
-        optionA: "30 hours",
-        optionB: "38 hours",
-        optionC: "48 hours",
-        optionD: "24 hours",
+        question: "Quel métier exerce le père de Peppa ?",
+        optionA: "Boucher",
+        optionB: "Ingénieur",
+        optionC: "Chasseur",
+        optionD: "Architecte",
         correctOption: "optionD"
     },
 
     {
-        question: "Which is the longest river in the world ?",
-        optionA: "River Nile",
-        optionB: "Long River",
-        optionC: "River Niger",
-        optionD: "Lake Chad",
-        correctOption: "optionA"
-    },
-
-    {
-        question: "_____ is the hottest Continent on Earth ?",
-        optionA: "Oceania",
-        optionB: "Antarctica",
-        optionC: "Africa",
-        optionD: "North America",
+        question: "Qui est la meilleure amie de Peppa",
+        optionA: "Suzy Dog",
+        optionB: "Suzy Rabbit",
+        optionC: "Suzy Sheep",
+        optionD: "Jean Dujardin",
         correctOption: "optionC"
     },
 
     {
-        question: "Which country is the largest in the world ?",
-        optionA: "Russia",
-        optionB: "Canada",
-        optionC: "Africa",
-        optionD: "Egypt",
-        correctOption: "optionA"
+        question: "Qui est le meilleur ami de George",
+        optionA: "Danny Dog",
+        optionB: "Richard Rabbit",
+        optionC: "Richard Sheep",
+        optionD: "Tom Cruise",
+        correctOption: "optionB"
     },
 
     {
-        question: "Which of these numbers is an odd number ?",
-        optionA: "Ten",
-        optionB: "Twelve",
-        optionC: "Eight",
-        optionD: "Eleven",
+        question: "Que fait la mère de Peppa comme métier",
+        optionA: "Elle joue sur son ordinateur",
+        optionB: "Elle travaille sur son ordinateur",
+        optionC: "Elle est babysitter",
+        optionD: "Elle est bouchère",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Combien d'épisodes de Peppa Pig y a t'il en tout ?",
+        optionA: "400",
+        optionB: "200",
+        optionC: "6 saisons",
+        optionD: "300",
         correctOption: "optionD"
     },
 
     {
-        question: `"You Can't see me" is a popular saying by`,
-        optionA: "Eminem",
-        optionB: "Bill Gates",
-        optionC: "Chris Brown",
+        question: "Quel type de fleur va devenir gigantesque dans l'episode des fleurs géantes?",
+        optionA: "Tournesol",
+        optionB: "Coquelicot",
+        optionC: "Rose",
         optionD: "John Cena",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "Where is the world tallest building located ?",
-        optionA: "Africa",
-        optionB: "California",
-        optionC: "Dubai",
-        optionD: "Italy",
-        correctOption: "optionC"
-    },
-
-    {
-        question: "The longest river in the United Kingdom is ?",
-        optionA: "River Severn",
-        optionB: "River Mersey",
-        optionC: "River Trent",
-        optionD: "River Tweed",
-        correctOption: "optionA"
-    },
-
-
-    {
-        question: "How many permanent teeth does a dog have ?",
-        optionA: "38",
-        optionB: "42",
-        optionC: "40",
-        optionD: "36",
-        correctOption: "optionB"
-    },
-
-    {
-        question: "Which national team won the football World cup in 2018 ?",
-        optionA: "England",
-        optionB: "Brazil",
-        optionC: "Germany",
-        optionD: "France",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "Which US state was Donald Trump Born ?",
-        optionA: "New York",
-        optionB: "California",
-        optionC: "New Jersey",
-        optionD: "Los Angeles",
         correctOption: "optionA"
     },
 
     {
-        question: "How man states does Nigeria have ?",
-        optionA: "24",
-        optionB: "30",
-        optionC: "36",
-        optionD: "37",
-        correctOption: "optionC"
-    },
-
-    {
-        question: "____ is the capital of Nigeria ?",
-        optionA: "Abuja",
-        optionB: "Lagos",
-        optionC: "Calabar",
-        optionD: "Kano",
-        correctOption: "optionA"
-    },
-
-    {
-        question: "Los Angeles is also known as ?",
-        optionA: "Angels City",
-        optionB: "Shining city",
-        optionC: "City of Angels",
-        optionD: "Lost Angels",
-        correctOption: "optionC"
-    },
-
-    {
-        question: "What is the capital of Germany ?",
-        optionA: "Georgia",
-        optionB: "Missouri",
-        optionC: "Oklahoma",
-        optionD: "Berlin",
+        question: "Quel âge a désormais peppa pig dans la vraie vie ?",
+        optionA: "64 ans",
+        optionB: "12 ans",
+        optionC: "6 ans",
+        optionD: "23 ans",
         correctOption: "optionD"
     },
 
     {
-        question: "How many sides does an hexagon have ?",
-        optionA: "Six",
-        optionB: "Sevene",
-        optionC: "Four",
-        optionD: "Five",
-        correctOption: "optionA"
-    },
-
-    {
-        question: "How many planets are currently in the solar system ?",
-        optionA: "Eleven",
-        optionB: "Seven",
-        optionC: "Nine",
-        optionD: "Eight",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "Which Planet is the hottest ?",
-        optionA: "Jupitar",
-        optionB: "Mercury",
-        optionC: "Earth",
-        optionD: "Venus",
-        correctOption: "optionB"
-    },
-
-    {
-        question: "where is the smallest bone in human body located?",
-        optionA: "Toes",
-        optionB: "Ears",
-        optionC: "Fingers",
-        optionD: "Nose",
-        correctOption: "optionB"
-    },
-
-    {
-        question: "How many hearts does an Octopus have ?",
-        optionA: "One",
-        optionB: "Two",
-        optionC: "Three",
-        optionD: "Four",
+        question: "Quel est l'une des activités préférées de Peppa ?",
+        optionA: "Manger du bacon",
+        optionB: "Sauter sur de la terre",
+        optionC: "Sauter dans la boue",
+        optionD: "Se recueillir dans des boucheries",
         correctOption: "optionC"
     },
 
-    {
-        question: "How many teeth does an adult human have ?",
-        optionA: "28",
-        optionB: "30",
-        optionC: "32",
-        optionD: "36",
-        correctOption: "optionC"
-    }
 
 ]
 
